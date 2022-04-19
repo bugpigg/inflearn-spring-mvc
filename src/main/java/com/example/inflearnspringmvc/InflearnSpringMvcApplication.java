af1e576c0b931cfeca82a9b2719ca41465cd242b
bugpigg <bugpigg@gmail.com>
2022-04-19 23:50:52 +0900

    Initial commit

---
+++ src/main/java/com/example/inflearnspringmvc/InflearnSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.inflearnspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnSpringMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/inflearnspringmvc/InflearnSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inflearnspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
